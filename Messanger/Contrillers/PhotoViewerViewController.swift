cfbe41c882b9b963f0658d6306620cc3b6f8501d
okGitHub <48615436+Pajamabrew@users.noreply.github.com>
2021-01-28 13:41:40 +0300

    1.0

---
+++ Messanger/Contrillers/PhotoViewerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PhotoViewerViewController.swift
+//  Messanger
+//
+//  Created by Petr on 05.10.2020.
+//
+
+import UIKit
+
+class PhotoViewerViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+}
